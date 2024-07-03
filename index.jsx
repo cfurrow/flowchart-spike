dd9e886c5531e9d2b8c387969ec094ea85ecc341
Carl Furrow <hello@carlfurrow.com>
2024-07-03 12:25:36 -0400

    import reactflow example file

--- index.jsx
+++ index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Flow from './Flow';
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+root.render(<Flow />);
